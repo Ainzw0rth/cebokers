0d8773ae78a8116fe7d0321576fbad300bd4f0c5
louis <louiscaesapdf@gmail.com>
2022-11-25 23:49:20 +0700

    revisi

--- CFGtoCNF.py
+++ CFGtoCNF.py
@@ -4,70 +4,68 @@ from grammar_reader import is_terminal
 # referensi : https://www.youtube.com/watch?v=7G0PwGrdlH8&ab_channel=Education4u
 
 def CFG_to_CNF(CFG):
-    # STEP 2: Remove unit productions.
-    contain_unit = True
+    # STEP 2 : JADIKAN SEBAGAI NORMAL FORM
+    ada = True
 
-    while contain_unit:
-        unit_productions = {}
-        contain_unit = False
+    while ada:
+        satuElemen = {}
+        ada = False
         
-        for head, body in CFG.items():
-            for rule in body:
+        for key, value in CFG.items():
+            for rule in value:
                 if len(rule) == 1 and isVariable(rule[0]):
-                    contain_unit = True
-                    if head not in unit_productions.keys():
-                        unit_productions[head] = [[rule[0]]]
+                    ada = True
+                    if key not in satuElemen.keys():
+                        satuElemen[key] = [[rule[0]]]
                     else:
-                        unit_productions[head].append([rule[0]])
+                        satuElemen[key].append([rule[0]])
 
-        for head_unit, body_unit in unit_productions.items():
-            for rule_unit in body_unit:
-                for head, body in CFG.items():
-                    if len(rule_unit) == 1 and head == rule_unit[0]:
-                        new_rule = {head_unit : body}
-                        if head_unit not in CFG.keys():
-                            CFG[head_unit] = body
+        for keySatuElemen, valueSatuElemen in satuElemen.items():
+            for rule_unit in valueSatuElemen:
+                for key, value in CFG.items():
+                    if len(rule_unit) == 1 and key == rule_unit[0]:
+                        if keySatuElemen not in CFG.keys():
+                            CFG[keySatuElemen] = value
                         else:
-                            for rule in body:
-                                if rule not in CFG[head_unit]:
-                                    CFG[head_unit].append(rule)
+                            for rule in value:
+                                if rule not in CFG[keySatuElemen]:
+                                    CFG[keySatuElemen].append(rule)
     
-        for head_unit, body_unit in unit_productions.items():
-            for rule_unit in body_unit:
+        for keySatuElemen, valueSatuElemen in satuElemen.items():
+            for rule_unit in valueSatuElemen:
                 if len(rule_unit) == 1:
-                    CFG[head_unit].remove(rule_unit)
+                    CFG[keySatuElemen].remove(rule_unit)
 
     # STEP 3: Replace Body with 3 or more Variables
-    new_productions = {}
+    RulesBaru = {}
     del_productions = {}
 
     i = 0
-    for head, body in CFG.items():
-        for rule in body:
-            head_symbol = head
+    for key, value in CFG.items():
+        for rule in value:
+            temp = key
             temp_rule = [r for r in rule]
-            if len(temp_rule) > 2:
-                while len(temp_rule) > 2:
-                    new_symbol = f"X{i}"
-                    if head_symbol not in new_productions.keys():
-                        new_productions[head_symbol] = [[temp_rule[0], new_symbol]]
-                    else:
-                        new_productions[head_symbol].append([temp_rule[0], new_symbol])
-                    head_symbol = new_symbol
-                    temp_rule.remove(temp_rule[0])
-                    i += 1
+            while len(temp_rule) > 2:
+                ruleTambahan  = f"Y{i}"
+                if temp not in RulesBaru.keys():
+                    RulesBaru[temp] = [[temp_rule[0], ruleTambahan ]]
                 else:
-                    if head_symbol not in new_productions.keys():
-                        new_productions[head_symbol] = [temp_rule]
-                    else:
-                        new_productions[head_symbol].append(temp_rule)
-                    
-                    if head not in del_productions.keys():
-                        del_productions[head] = [rule]
-                    else:
-                        del_productions[head].append(rule)
+                    RulesBaru[temp].append([temp_rule[0], ruleTambahan ])
+                temp = ruleTambahan 
+                temp_rule.remove(temp_rule[0])
+                i += 1
+            else:
+                if temp not in RulesBaru.keys():
+                    RulesBaru[temp] = [temp_rule]
+                else:
+                    RulesBaru[temp].append(temp_rule)
+                
+                if key not in del_productions.keys():
+                    del_productions[key] = [rule]
+                else:
+                    del_productions[key].append(rule)
 
-    for new_head, new_body in new_productions.items():
+    for new_head, new_body in RulesBaru.items():
         if new_head not in CFG.keys():
             CFG[new_head] = new_body
         else:
@@ -78,77 +76,40 @@ def CFG_to_CNF(CFG):
             CFG[del_head].remove(del_rule)
 
     # STEP 4: Replace Terminal adjacent to a Variables
-    new_productions = {}
-    del_productions = {}
+    RulesBaru = {}
+    RulesLama = {}
 
-    j = 0
-    k = 0
+    i = 0
     for head, body in CFG.items():
         for rule in body:
-            if len(rule) == 2 and is_terminal(rule[0]) and is_terminal(rule[1]):
-                new_symbol_Y = f"Y{j}"
-                new_symbol_Z = f"Z{k}"
+            temp = []
+            ada = False
+            for r in rule:
+                temp.append(r)
 
-                if head not in new_productions.keys():
-                    new_productions[head] = [[new_symbol_Y, new_symbol_Z]]
+            for x in range(len(rule)):
+                if is_terminal(rule[x]):
+                    ada = True
+                    simbolbaruX = f"X{i}"
+                    i += 1
+                    RulesBaru[simbolbaruX] = [[rule[x]]]
+                    temp[x] = simbolbaruX
+
+            if ada:
+                if head not in RulesBaru.keys():
+                    RulesBaru[head] = [temp]
                 else:
-                    new_productions[head].append([new_symbol_Y, new_symbol_Z])
-                    
-                new_productions[new_symbol_Y] = [[rule[0]]]
-                new_productions[new_symbol_Z] = [[rule[1]]]
-
-                if head not in del_productions.keys():
-                    del_productions[head] = [rule]
-                else:
-                    del_productions[head].append(rule)
-
-                j += 1
-                k += 1
-
-            elif len(rule) == 2 and is_terminal(rule[0]):
-                new_symbol_Y = f"Y{j}"
-
-                if head not in new_productions.keys():
-                    new_productions[head] = [[new_symbol_Y, rule[1]]]
-                else:
-                    new_productions[head].append([new_symbol_Y, rule[1]])
-
-                new_productions[new_symbol_Y] = [[rule[0]]]
-
-                if head not in del_productions.keys():
-                    del_productions[head] = [rule]
-                else:
-                    del_productions[head].append(rule)
-
-                j += 1
-
-            elif len(rule) == 2 and is_terminal(rule[1]):
-                new_symbol_Z = f"Z{k}"
-
-                if head not in new_productions.keys():
-                    new_productions[head] = [[rule[0], new_symbol_Z]]
-                else:
-                    new_productions[head].append([rule[0], new_symbol_Z])
-
-                new_productions[new_symbol_Z] = [[rule[1]]]
-
-                if head not in del_productions.keys():
-                    del_productions[head] = [rule]
-                else:
-                    del_productions[head].append(rule)
-
-                k += 1
-
+                    RulesBaru[head].append(temp)
             else:
                 pass
 
-    for new_head, new_body in new_productions.items():
+    for new_head, new_body in RulesBaru.items():
         if new_head not in CFG.keys():
             CFG[new_head] = new_body
         else:
             CFG[new_head].extend(new_body)
 
-    for del_head, del_body in del_productions.items():
+    for del_head, del_body in RulesLama.items():
         for del_rule in del_body:
             CFG[del_head].remove(del_rule)
 
